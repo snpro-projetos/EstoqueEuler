fbb3394167406943f6f6a2c2c62806ad77ae511f
IanRezende77 <12500763@aluno.cotemig.com.br>
2026-07-03 15:56:51 -0300

    atualizado

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\EstoqueMaster_SNPro_SemValores\EstoqueMaster\venv"
+set "VIRTUAL_ENV=C:\Users\ian.rezende\Downloads\EstoqueMaster_SNPro_Equipamentos\EstoqueMaster\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
